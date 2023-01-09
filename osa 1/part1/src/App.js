e0d3695242ed0f71689c9950d27ee39db2d09319
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-01-09 22:57:17 +0200

    assignment 1.2: kurssitiedot, step2

--- osa 1/part1/src/App.js
+++ osa 1/part1/src/App.js
@@ -6,37 +6,35 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+
   const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Part = (props) => {
     return (
-      <>
-        <h1>{props.course}</h1>
-      </>
+      <p>
+        {props.part} {props.exercises}
+      </p>
     );
   };
+
   const Content = (props) => {
     return (
-      <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
-      </>
+      <div>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
+      </div>
     );
   };
 
   const Total = (props) => {
     return (
-      <>
-        <p>
-          Number of exercises{" "}
-          {props.exercises1 + props.exercises2 + props.exercises3}
-        </p>
-      </>
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
     );
   };
 
