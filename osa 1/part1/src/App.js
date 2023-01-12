4aacccfc1ab5c05a663aa3096229ef17190fe514
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-01-12 12:40:47 +0200

    moved all components from inside of App component to outsie of App compnent

--- osa 1/part1/src/App.js
+++ osa 1/part1/src/App.js
@@ -1,3 +1,36 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
+};
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
@@ -15,39 +48,6 @@ const App = () => {
         exercises: 14,
       },
     ],
-  };
-
-  const Header = (props) => {
-    return <h1>{props.course}</h1>;
-  };
-
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    );
-  };
-
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-      </div>
-    );
-  };
-
-  const Total = (props) => {
-    return (
-      <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
-    );
   };
 
   return (
