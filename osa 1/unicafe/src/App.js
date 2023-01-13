49341534c50e64e9fe8281acb21ae73352598203
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-01-13 11:31:26 +0200

    assignment 1.7: ubicafe step3

--- osa 1/unicafe/src/App.js
+++ osa 1/unicafe/src/App.js
@@ -4,30 +4,78 @@ const Headers = (props) => {
   return <h1>{props.header}</h1>;
 };
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
-
 const App = () => {
   const header = {
     topText: "give feedback",
     bottomText: "statistics",
   };
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
 
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+    average: 0,
+  });
+
+  const handleGoodClick = () => {
+    const newClicks = {
+      ...clicks,
+      good: clicks.good + 1,
+      all: clicks.all + 1,
+      average: clicks.average + 1,
+    };
+    setClicks(newClicks);
+  };
+
+  const handleNeutralClick = () => {
+    const newClicks = {
+      ...clicks,
+      neutral: clicks.neutral + 1,
+      all: clicks.all + 1,
+    };
+    setClicks(newClicks);
+  };
+
+  const handleBadClick = () => {
+    const newClicks = {
+      ...clicks,
+      bad: clicks.bad + 1,
+      all: clicks.all + 1,
+      average: clicks.average - 1,
+    };
+    setClicks(newClicks);
+  };
+  if (clicks.all === 0) {
+    return (
+      <div>
+        <Headers header={header.topText} />
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
+        <Headers header={header.bottomText} />
+        <p>good {clicks.good}</p>
+        <p>neutral {clicks.neutral}</p>
+        <p>bad {clicks.bad}</p>
+        <p>all {clicks.all}</p>
+        <p>average {clicks.average}</p>
+        <p>positive 0</p>
+      </div>
+    );
+  }
   return (
     <div>
       <Headers header={header.topText} />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <Headers header={header.bottomText} />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {clicks.good}</p>
+      <p>neutral {clicks.neutral}</p>
+      <p>bad {clicks.bad}</p>
+      <p>all {clicks.all}</p>
+      <p>average {clicks.average / clicks.all}</p>
+      <p>positive {(clicks.good / clicks.all) * 100}</p>
     </div>
   );
 };
