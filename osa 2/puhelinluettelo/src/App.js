31afbbd4a7aed266a8a077245ac6c6db69ab6d56
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-01-25 14:04:03 +0200

    2.9*: puhelinluettelo step4

--- osa 2/puhelinluettelo/src/App.js
+++ osa 2/puhelinluettelo/src/App.js
@@ -3,9 +3,13 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1231244" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchText, setSearchText] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -25,22 +29,27 @@ const App = () => {
     setNewNumber("");
   };
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          filter shown with:
+          <input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          name:
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,11 +57,24 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {persons
+        .filter((person) => {
+          if (searchText === "") {
+            return person;
+          } else if (
+            person.name
+              .toLocaleLowerCase()
+              .includes(searchText.toLocaleLowerCase())
+          ) {
+            return person;
+          }
+          return null;
+        })
+        .map((person) => (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))}
     </div>
   );
 };
