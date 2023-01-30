6f7d3f8d9c149044c84afd64e3ab405f3294a5c2
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-01-30 16:20:22 +0200

    3.4 puhelinluettelon backend step4

--- osa 3/api/index.js
+++ osa 3/api/index.js
@@ -45,6 +45,18 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  person = persons.filter((person) => person.id !== id);
+
+  if (!person) {
+    res.status(404).end();
+  } else {
+    console.log(`Deleted person with id ${id}`);
+    res.status(204).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
