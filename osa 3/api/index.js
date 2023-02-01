428df6c9de18b3d100f10a462b80b15050d664a5
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-02-01 19:45:54 +0200

    3.15: puhelinluettelo ja tietokanta, step 3

--- osa 3/api/index.js
+++ osa 3/api/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const Person = require("./models/person");
 
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.static("build"));
@@ -85,17 +85,26 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  console.log(`Deleted person with id ${id}`);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT || 3001;
