68c0be9cb2ce0bea448fc7d95db73c3b645de17d
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-02-23 15:00:11 +0200

    4.19: blogilistan laajennus, step7

--- osa 4/api/controllers/blog.js
+++ osa 4/api/controllers/blog.js
@@ -1,16 +1,29 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  const { title, author, url, likes, userId } = request.body;
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
-  const user = await User.findById(userId);
+blogsRouter.post("/", async (request, response, next) => {
+  const { title, author, url, likes } = request.body;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: title,
