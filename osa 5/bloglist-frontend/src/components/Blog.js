522e483a361356971f07cfb3ecad883249a710ed
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-03-02 13:20:22 +0200

    5.7: blogilistan frontend, step 7

--- osa 5/bloglist-frontend/src/components/Blog.js
+++ osa 5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-);
+import { useState } from "react";
+import "./Blog.css";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [showAllInfo, setShowAllInfo] = useState(false);
+  return (
+    <div style={blogStyle}>
+      {!showAllInfo ? (
+        <ul className="blog__info">
+          <li>
+            {blog.title}
+            <button type="button" onClick={() => setShowAllInfo(!showAllInfo)}>
+              view
+            </button>
+          </li>
+        </ul>
+      ) : (
+        <ul className="blog__info">
+          <li>
+            {blog.title}
+            <button type="button" onClick={() => setShowAllInfo(!showAllInfo)}>
+              hide
+            </button>
+          </li>
+          <li>{blog.url}</li>
+          <li>
+            likes {blog.likes} <button type="button">like</button>
+          </li>
+          <li>{blog.author}</li>
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
