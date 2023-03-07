3a44c7939220d911a8463ea1f7ac75512d29e37b
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-03-07 13:07:27 +0200

    5.18: blogilistan end to end -testit, step 2

--- osa 5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa 5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,7 +2,7 @@ describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
     const user = {
-      name: "Joni Vepsäläinen",
+      name: "Ted Tester",
       username: "tester",
       password: "test2023",
     };
@@ -18,5 +18,25 @@ describe("Blog app", function () {
     cy.get("#password").should("have.value", "");
     cy.get("#login-button").should("have.text", "login");
     cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.visit("http://localhost:3000");
+      cy.get("#username").type("tester");
+      cy.get("#password").type("test2023");
+      cy.get("#login-button").click();
+      cy.contains("tester logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.visit("http://localhost:3000");
+      cy.get("#username").type("tester");
+      cy.get("#password").type("test202");
+      cy.get("#login-button").click();
+      cy.get(".error").should("contain", "invalid username or password");
+      cy.get(".error").should("have.css", "color", "rgb(255, 0, 0)");
+      cy.get(".error").should("have.css", "border-style", "solid");
+    });
   });
 });
