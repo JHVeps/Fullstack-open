ebc4c43c292e4ff160c5c6a855753e5d187bdeb4
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-03-13 12:22:54 +0200

    6.1: Unicafe revisited, step1

--- osa 6/unicafe-redux/src/reducer.js
+++ osa 6/unicafe-redux/src/reducer.js
@@ -1,25 +1,23 @@
 const initialState = {
-    good: 0,
-    ok: 0,
-    bad: 0
+  good: 0,
+  ok: 0,
+  bad: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "GOOD":
+      return { ...state, good: state.good + 1 };
+    case "OK":
+      return { ...state, ok: state.ok + 1 };
+    case "BAD":
+      return { ...state, bad: state.bad + 1 };
+    case "ZERO":
+      return state;
+    default:
+      return state;
   }
-  
-  const counterReducer = (state = initialState, action) => {
-    console.log(action)
-    switch (action.type) {
-      case 'GOOD':
-        state.good + 1
-        return state
-      case 'OK':
-        state.ok + 1
-        return state
-      case 'BAD':
-        state.bad +1
-        return state
-      case 'ZERO':
-        return state
-    }
-    return state
-  }
-  
-  export default counterReducer
+};
+
+export default counterReducer;
