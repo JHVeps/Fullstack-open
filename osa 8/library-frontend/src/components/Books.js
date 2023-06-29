6bcb328b7d2081e4ec7cfad4e72c9763c77be3a6
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-06-29 22:45:20 +0300

    8.17 Kirjojen lista

--- osa 8/library-frontend/src/components/Books.js
+++ osa 8/library-frontend/src/components/Books.js
@@ -19,11 +19,11 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {result.data.allBooks.map((b) => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
