de4a2df3d31a940ae4495ee63f7435b58f702dfc
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-06-30 15:13:07 +0300

    8.19 Genren kirjat

--- osa 8/library-frontend/src/components/Books.js
+++ osa 8/library-frontend/src/components/Books.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import { ALL_BOOKS } from "../queries";
 import { useQuery } from "@apollo/client";
 
 const Books = () => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
   const result = useQuery(ALL_BOOKS);
+  let genres = ["all genres"];
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  // Collect genres from allBooks
+  result.data.allBooks.forEach((book) => {
+    genres = [...genres, ...book.genres];
+  });
+
+  // Remove duplicates and sort the genres array
+  const uniqueGenres = Array.from(new Set(genres)).sort();
+
+  const handleGenreClick = (genre) => {
+    setSelectedGenre(genre === "all genres" ? null : genre);
+  };
+
+  const filteredBooks = selectedGenre
+    ? result.data.allBooks.filter((book) => book.genres.includes(selectedGenre))
+    : result.data.allBooks;
+
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -19,7 +37,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map((b) => (
+          {filteredBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -28,6 +46,20 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+      <div>
+        <h3>Genres:</h3>
+        {uniqueGenres.map((genre) => (
+          <button
+            key={genre}
+            onClick={() => handleGenreClick(genre)}
+            style={{
+              fontWeight: selectedGenre === genre ? "bold" : "normal",
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
