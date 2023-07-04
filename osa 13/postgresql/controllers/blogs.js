a1f5780c08d4e36bf9a4c8412347bed3cc5b12e7
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-07-04 19:18:57 +0300

    Tehtävä 13.10.

--- osa 13/postgresql/controllers/blogs.js
+++ osa 13/postgresql/controllers/blogs.js
@@ -1,5 +1,24 @@
 const router = require("express").Router();
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
+
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      console.log(authorization.substring(7));
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
 
 //middleware for finding blog
 const blogFinder = async (req, res, next) => {
@@ -12,8 +31,16 @@ router.get("/", async (req, res) => {
   res.json(blogs);
 });
 
-router.post("/", async (req, res) => {
-  const blog = await Blog.create(req.body);
+router.post("/", tokenExtractor, async (req, res) => {
+  console.log(req.body);
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await Blog.create({
+    ...req.body,
+    userId: user.id,
+    date: new Date(),
+  });
+  //const blog = await Blog.create(req.body);
+  console.log(blog);
   res.json(blog);
 });
 
