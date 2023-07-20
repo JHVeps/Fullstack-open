9f59d335c51c7c9910bf08591da330916a399371
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-07-20 18:42:14 +0300

    Tehtävä 13.23.

--- osa 13/postgresql/controllers/users.js
+++ osa 13/postgresql/controllers/users.js
@@ -29,31 +29,89 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id, {
-    attributes: { exclude: ["created_at", "updated_at"] },
+  const { read } = req.query;
 
-    include: [
-      {
-        model: Blog,
-        attributes: {
-          exclude: ["created_at", "updated_at", "userId", "isRead"],
-        },
-        as: "Readings", // Use the alias "Readings" for the many-to-many association
-        through: {
-          attributes: ["id", "read"], // Include only the id property from the through table (readlist)
-        },
-        include: [
-          {
-            model: User,
-            attributes: ["username", "name"], // Include only the desired attributes from the User model
-            as: "Adder", // Use the alias "Adder" for the belongsTo association
+  if (read === "true") {
+    // If the read=true parameter is present, filter blogs where read=true
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["created_at", "updated_at"] },
+      include: [
+        {
+          model: Blog,
+          attributes: {
+            exclude: ["created_at", "updated_at", "userId", "isRead"],
           },
-        ],
-      },
-    ],
-  });
-
-  res.json(user);
+          as: "Readings",
+          through: {
+            attributes: ["id", "read"],
+          },
+          include: [
+            {
+              model: User,
+              attributes: ["username", "name"],
+              as: "Adder",
+            },
+          ],
+          where: {
+            "$Readings->readlist.read$": true,
+          },
+        },
+      ],
+    });
+    res.json(user);
+  } else if (read === "false") {
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["created_at", "updated_at"] },
+      include: [
+        {
+          model: Blog,
+          attributes: {
+            exclude: ["created_at", "updated_at", "userId", "isRead"],
+          },
+          as: "Readings",
+          through: {
+            attributes: ["id", "read"],
+          },
+          include: [
+            {
+              model: User,
+              attributes: ["username", "name"],
+              as: "Adder",
+            },
+          ],
+          where: {
+            "$Readings->readlist.read$": false,
+          },
+        },
+      ],
+    });
+    res.json(user);
+  } else {
+    // If the read=true/false parameter is not present or set to false, fetch all blogs
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["created_at", "updated_at"] },
+      include: [
+        {
+          model: Blog,
+          attributes: {
+            exclude: ["created_at", "updated_at", "userId", "isRead"],
+          },
+          as: "Readings",
+          through: {
+            attributes: ["id", "read"],
+          },
+          include: [
+            {
+              model: User,
+              attributes: ["username", "name"],
+              as: "Adder",
+            },
+          ],
+        },
+      ],
+    });
+    res.json(user);
+  }
 });
 
 router.put("/:username", async (req, res) => {
