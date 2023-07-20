6602e08552784fd55d365feba11b4d4267b6cce4
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-07-20 18:08:06 +0300

    Tehtävä 13.22.

--- osa 13/postgresql/controllers/readinglist.js
+++ osa 13/postgresql/controllers/readinglist.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
-const { ReadList } = require("../models");
+const { ReadList, User } = require("../models");
+
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
+
+const { tokenExtractor } = require("../util/middlewares");
+
+//middleware for finding blog
+const readListFinder = async (req, res, next) => {
+  req.readlist = await ReadList.findByPk(req.params.id);
+  next();
+};
 
 router.post("/", async (req, res) => {
   try {
@@ -12,6 +23,17 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error("Error creating readlist entry:", error);
     res.status(500).json({ error: "Failed to create readlist entry." });
+  }
+});
+
+router.put("/:id", readListFinder, tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  if (user.id === req.readlist.userId) {
+    req.readlist.read = req.body.read;
+    await req.readlist.save();
+    res.json(req.readlist);
+  } else {
+    res.status(404).end();
   }
 });
 
